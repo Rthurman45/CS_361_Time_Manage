0204d959bcdd356943e05da7090833330f71b7d1
robert <robertqthurman@gmail.com>
2026-07-19 17:35:44 -0700

    starting

--- time_app.py
+++ time_app.py
@@ -1,1 +1,20 @@
 #Robert Thurman
+
+"""
+starting skeleton.
+Fill in routes below as features are built.
+"""
+from fastapi import FastAPI
+ 
+app = FastAPI(title="tagalong")
+ 
+# TODO: signup / login
+# TODO: post activity
+# TODO: feed
+# TODO: join / leave activity
+ 
+ 
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+ 
